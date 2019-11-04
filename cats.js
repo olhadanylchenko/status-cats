a3b03bf9419b1464d397bdccf90bbc347de3bddf
Olha Danylchenko <olhadanylchenko@MacBook-Air-Olha.local>
2019-11-04 14:48:18 +0100

    made onClick function, pictures are shown on the same page

--- cats.js
+++ cats.js
@@ -1,6 +1,22 @@
-function clicking() {
-    let mynumber = document.getElementById("myNumber").value;
-    console.log(mynumber);
-    window.location.assign(`https://http.cat/${mynumber}`);
-}
+// function clicking() {
+//     let mynumber = document.getElementById("myNumber").value;
+//     console.log(mynumber);
+//     window.location.assign(`https://http.cat/${mynumber}`);
+// };
 
+
+
+
+$(document).ready(function () {
+    $("#show").click(function () {
+        let mynumber = document.getElementById("myNumber").value;
+        $("#main").empty();
+        $("#main").append(`<img src="https://http.cat/${mynumber}.jpg" class="img-fluid" id="kitty"/>`);
+
+
+
+
+        // console.log(mynumber);
+        // window.location.assign(`https://http.cat/${mynumber}`)
+    })
+});
